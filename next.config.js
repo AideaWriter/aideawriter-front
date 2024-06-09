94277051ac1209b11d4f3e904eef550f252d5b6d
Arsen <arsen@getmeva.com>
2024-06-09 10:18:36 +0500

    change price id pro

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        PRICE_ID_PRO: "price_1P52uyH0C2HpMlPZFo0UQM9j",
+        PRICE_ID_PRO: "price_1PPe3mH0C2HpMlPZivvCgnTT",
         PRICE_ID_PRO_PLUS: "price_1P6VpTH0C2HpMlPZ9BrS1tCL"
     }
 }
